eca8619e721e82f23ad14c7683a5297c06e102bd
ayman <ayman@gmail.com>
2018-10-11 05:14:08 +0200

    Loading Screen done...

--- app/src/main/java/com/example/ayman/cherish/activities/LoadingScreen.java
+++ app/src/main/java/com/example/ayman/cherish/activities/LoadingScreen.java
@@ -8,6 +8,8 @@ import android.view.WindowManager;
 
 import com.example.ayman.cherish.R;
 
+import java.util.Timer;
+
 public class LoadingScreen extends AppCompatActivity {
 	
 	private static int splashScreen=3000;
@@ -16,25 +18,14 @@ public class LoadingScreen extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.loading_screen);
 		
-		//to full screen without status bar
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
-		//TODO:Delay for loading screen
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				startAct();
-				this.finish();
-			}
-
-			private void finish()
-			{
-
-			}
-		},splashScreen);
+		startAct();
 		
 	}
 	
@@ -42,8 +33,8 @@ public class LoadingScreen extends AppCompatActivity {
 	{
 		Intent in=new Intent(this,SplashScreen.class);
 //		//TODO:TO CLEAR ACTIVITY STACK
-		in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//		in.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 		startActivity(in);
-//		finish();
+		finish();
 	}
 }
--- app/src/main/java/com/example/ayman/cherish/activities/SplashScreen.java
+++ app/src/main/java/com/example/ayman/cherish/activities/SplashScreen.java
@@ -1,5 +1,6 @@
 package com.example.ayman.cherish.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,7 @@ public class SplashScreen extends AppCompatActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		setTheme(R.style.AppTheme);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cherish);
 	}
