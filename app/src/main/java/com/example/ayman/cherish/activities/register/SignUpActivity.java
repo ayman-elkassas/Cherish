f1bfcac56c36d7781dba5be7afa1206d2bd7c0a5
ayman <aymanelkassas8@gmail.com>
2018-10-12 17:57:57 +0200

    Registering Activities done....

--- app/src/main/java/com/example/ayman/cherish/activities/register/SignUpActivity.java
+++ app/src/main/java/com/example/ayman/cherish/activities/register/SignUpActivity.java
@@ -5,13 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.ayman.cherish.R;
+import com.example.ayman.cherish.activities.accountSetup.AccountSetup;
 
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 	
 	private TextView haveAccount;
+	private Button signUp;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +26,11 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 		//WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		
 		haveAccount=findViewById(R.id.haveAccount);
+		signUp=findViewById(R.id.signUp);
+		
+		//listener
 		haveAccount.setOnClickListener(this);
+		signUp.setOnClickListener(this);
 		
 	}
 	
@@ -32,8 +39,11 @@ public class SignUpActivity extends AppCompatActivity implements View.OnClickLis
 		switch (v.getId())
 		{
 			case R.id.haveAccount:
-//				Intent in=new Intent(this,SignInActivity.class);
-//				startActivity(in);
+				finish();
+				break;
+			case R.id.signUp:
+				Intent in=new Intent(this,AccountSetup.class);
+				startActivity(in);
 				finish();
 				break;
 			default:
