f38f073d8fbf7e0dae9196032eb26e597d288785
ayman <aymanelkassas8@gmail.com>
2018-10-16 08:03:56 +0200

    Profile Activity done....

--- app/src/main/java/com/example/ayman/cherish/activities/Profile/Profile.java
+++ app/src/main/java/com/example/ayman/cherish/activities/Profile/Profile.java
@@ -9,14 +9,20 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 import com.example.ayman.cherish.R;
 
+import de.hdodenhof.circleimageview.CircleImageView;
+
 public class Profile extends AppCompatActivity
 		implements NavigationView.OnNavigationItemSelectedListener {
+	
+	CircleImageView avatar;
+	DrawerLayout drawer;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,21 +31,21 @@ public class Profile extends AppCompatActivity
 		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 		setSupportActionBar(toolbar);
 		
-		FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-		fab.setOnClickListener(new View.OnClickListener() {
+		avatar=findViewById(R.id.avatar);
+		
+		drawer= (DrawerLayout) findViewById(R.id.drawer_layout);
+//		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+//				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//		drawer.addDrawerListener(toggle);
+//		toggle.syncState();
+		
+		avatar.setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void onClick(View view) {
-				Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-						.setAction("Action", null).show();
+			public void onClick(View v) {
+				drawer.openDrawer(Gravity.LEFT);
 			}
 		});
-		
-		DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-		ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-				this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-		drawer.addDrawerListener(toggle);
-		toggle.syncState();
-		
+
 		NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
 		navigationView.setNavigationItemSelectedListener(this);
 	}
@@ -52,28 +58,6 @@ public class Profile extends AppCompatActivity
 		} else {
 			super.onBackPressed();
 		}
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.profile, menu);
-		return true;
-	}
-	
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		
-		//noinspection SimplifiableIfStatement
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		
-		return super.onOptionsItemSelected(item);
 	}
 	
 	@SuppressWarnings("StatementWithEmptyBody")
