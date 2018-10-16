a80c5245772333686b3e3cbccb589da057ea68e8
ayman <aymanelkassas8@gmail.com>
2018-10-17 01:11:02 +0200

    Profile Activity done....

--- app/src/main/java/com/example/ayman/cherish/activities/Profile/Profile.java
+++ app/src/main/java/com/example/ayman/cherish/activities/Profile/Profile.java
@@ -1,13 +1,18 @@
 package com.example.ayman.cherish.activities.Profile;
 
 import android.graphics.Color;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -19,6 +24,7 @@ import android.widget.TextView;
 import com.example.ayman.cherish.R;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 import devlight.io.library.ntb.NavigationTabBar;
@@ -33,8 +39,8 @@ public class Profile extends AppCompatActivity
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_profile);
-		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-		setSupportActionBar(toolbar);
+//		Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//		setSupportActionBar(toolbar);
 		
 		avatar=findViewById(R.id.avatar);
 		
@@ -92,12 +98,13 @@ public class Profile extends AppCompatActivity
 		return true;
 	}
 	
+	
 	private void initUI() {
 		final ViewPager viewPager = (ViewPager) findViewById(R.id.vp_horizontal_ntb);
 		viewPager.setAdapter(new PagerAdapter() {
 			@Override
 			public int getCount() {
-				return 4;
+				return 5;
 			}
 			
 			@Override
@@ -113,10 +120,15 @@ public class Profile extends AppCompatActivity
 			@Override
 			public Object instantiateItem(final ViewGroup container, final int position) {
 				final View view = LayoutInflater.from(
-						getBaseContext()).inflate(R.layout.item_vp, null, false);
+						getBaseContext()).inflate(R.layout.item_vp_list, null, false);
 				
-//				final TextView txtPage = (TextView) view.findViewById(R.id.txt_vp_item_page);
-//				txtPage.setText(String.format("Page #%d", position));
+				final RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.rv);
+				recyclerView.setHasFixedSize(true);
+				recyclerView.setLayoutManager(new LinearLayoutManager(
+								getBaseContext(), LinearLayoutManager.VERTICAL, false
+						)
+				);
+				recyclerView.setAdapter(new RecycleAdapter());
 				
 				container.addView(view);
 				return view;
@@ -129,7 +141,7 @@ public class Profile extends AppCompatActivity
 		final ArrayList<NavigationTabBar.Model> models = new ArrayList<>();
 		models.add(
 				new NavigationTabBar.Model.Builder(
-						getResources().getDrawable(R.drawable.timeline),
+						getResources().getDrawable(R.drawable.timeline3),
 						Color.parseColor(colors[1]))
 //						.selectedIcon(getResources().getDrawable(R.drawable.timeline2))
 						.title("Timeline")
@@ -156,6 +168,15 @@ public class Profile extends AppCompatActivity
 		);
 		models.add(
 				new NavigationTabBar.Model.Builder(
+						getResources().getDrawable(R.drawable.bell),
+						Color.parseColor(colors[1]))
+//                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
+						.title("Notifications")
+//						.badgeTitle("icon")
+						.build()
+		);
+		models.add(
+				new NavigationTabBar.Model.Builder(
 						getResources().getDrawable(R.drawable.settings),
 						Color.parseColor(colors[1]))
 //                        .selectedIcon(getResources().getDrawable(R.drawable.ic_eighth))
@@ -166,6 +187,20 @@ public class Profile extends AppCompatActivity
 		
 		navigationTabBar.setModels(models);
 		navigationTabBar.setViewPager(viewPager, 0);
+		
+		//IMPORTANT: ENABLE SCROLL BEHAVIOUR IN COORDINATOR LAYOUT
+		navigationTabBar.setBehaviorEnabled(true);
+		
+		navigationTabBar.setOnTabBarSelectedIndexListener(new NavigationTabBar.OnTabBarSelectedIndexListener() {
+			@Override
+			public void onStartTabSelected(final NavigationTabBar.Model model, final int index) {
+			}
+			
+			@Override
+			public void onEndTabSelected(final NavigationTabBar.Model model, final int index) {
+				model.hideBadge();
+			}
+		});
 		navigationTabBar.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 			@Override
 			public void onPageScrolled(final int position, final float positionOffset, final int positionOffsetPixels) {
@@ -174,7 +209,7 @@ public class Profile extends AppCompatActivity
 			
 			@Override
 			public void onPageSelected(final int position) {
-				navigationTabBar.getModels().get(position).hideBadge();
+			
 			}
 			
 			@Override
@@ -183,21 +218,70 @@ public class Profile extends AppCompatActivity
 			}
 		});
 		
-		navigationTabBar.postDelayed(new Runnable() {
+		final CoordinatorLayout coordinatorLayout = (CoordinatorLayout) findViewById(R.id.parent);
+		findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
 			@Override
-			public void run() {
+			public void onClick(final View v) {
 				for (int i = 0; i < navigationTabBar.getModels().size(); i++) {
 					final NavigationTabBar.Model model = navigationTabBar.getModels().get(i);
 					navigationTabBar.postDelayed(new Runnable() {
 						@Override
 						public void run() {
-							model.showBadge();
+							final String title = String.valueOf(new Random().nextInt(15));
+							if (!model.isBadgeShowed()) {
+								model.setBadgeTitle(title);
+								model.showBadge();
+							} else model.updateBadgeTitle(title);
 						}
 					}, i * 100);
 				}
+				
+				coordinatorLayout.postDelayed(new Runnable() {
+					@Override
+					public void run() {
+						final Snackbar snackbar = Snackbar.make(navigationTabBar, "Coordinator NTB", Snackbar.LENGTH_SHORT);
+						snackbar.getView().setBackgroundColor(Color.parseColor("#9b92b3"));
+						((TextView) snackbar.getView().findViewById(R.id.snackbar_text))
+								.setTextColor(Color.parseColor("#423752"));
+						snackbar.show();
+					}
+				}, 1000);
 			}
-		}, 500);
+		});
+		
+		final CollapsingToolbarLayout collapsingToolbarLayout =
+				(CollapsingToolbarLayout) findViewById(R.id.toolbar);
+		collapsingToolbarLayout.setExpandedTitleColor(Color.parseColor("#009F90AF"));
+		collapsingToolbarLayout.setCollapsedTitleTextColor(Color.parseColor("#9f90af"));
 	}
 	
+	public class RecycleAdapter extends RecyclerView.Adapter<RecycleAdapter.ViewHolder> {
+		
+		@Override
+		public ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+			final View view = LayoutInflater.from(getBaseContext()).inflate(R.layout.item_list, parent, false);
+			return new ViewHolder(view);
+		}
+		
+		@Override
+		public void onBindViewHolder(final ViewHolder holder, final int position) {
+			holder.txt.setText(String.format("Navigation Item #%d", position));
+		}
+		
+		@Override
+		public int getItemCount() {
+			return 20;
+		}
+		
+		public class ViewHolder extends RecyclerView.ViewHolder {
+			
+			public TextView txt;
+			
+			public ViewHolder(final View itemView) {
+				super(itemView);
+				txt = (TextView) itemView.findViewById(R.id.txt_vp_item_list);
+			}
+		}
+	}
 }
 
